1a19dfededf9b96bc03853291e2a678e40bbc3b7
kauboimausu <malagonngasca@gmail.com>
2026-05-25 13:50:07 -0600

    cambios para la nube

---
+++ run_megan-lsgan-reduced2_train.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+python -u train.py --save_path runs/megan_lsgan_reduced2_cloud \
+	--clean_trainset data/clean_trainset \
+	--noisy_trainset data/noisy_trainset \
+    --clean_valset data/clean_valset \
+    --noisy_valset data/noisy_valset \
+    --epoch 50 \
+    --batch_size 100 \
+    --genc_fmaps    64 128 256 512 \
+    --genc_poolings 4  4   4   4   \
+    --gdec_poolings 4  4   4   4   \
+    --denc_fmaps    64 128 256 512 \
+    --denc_poolings 4  4   4   4   \
+    --dpool_slen 64 \
+	--no_train_gen --no_bias \
+    --num_workers 4 --cache_dir data_tmp \
+
---
+++ run_megan-lsgan_train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+python -u train.py --save_path runs/megan_lsgan_cloud \
+	--clean_trainset data/clean_trainset \
+	--noisy_trainset data/noisy_trainset \
+    --clean_valset data/clean_valset \
+    --noisy_valset data/noisy_valset \
+    --epoch 50 \
+    --batch_size 100 \
+    --genc_fmaps    64 128 256 512 1024 2048 \
+    --genc_poolings 4  4   4   4   2    2    \
+    --gdec_poolings 2  2   4   4   4    4    \
+    --denc_fmaps    64 128 256 512 1024 2048 \
+    --denc_poolings 4  4   4   4   2    2    \
+	--no_train_gen --no_bias \
+    --num_workers 4 --cache_dir data_tmp \
+
