926c4de198a6b55ac970cb10b255aeeb7c790707
kauboimausu <malagonngasca@gmail.com>
2026-05-25 22:22:14 -0600

    cambio de hiperparametros

--- run_megan-lsgan_train.sh
+++ run_megan-lsgan_train.sh
@@ -6,14 +6,16 @@ python -u train.py --save_path runs/megan_lsgan_cloud \
 	--noisy_trainset data/noisy_trainset \
     --clean_valset data/clean_valset \
     --noisy_valset data/noisy_valset \
-    --epoch 100 \
+    --epoch 86 \
     --max_samples 1500 \
-    --batch_size 32 \
+    --batch_size 50 \
+    --g_lr 0.0002 \
+    --d_lr 0.0002 \
     --genc_fmaps    64 128 256 512 1024 2048 \
     --genc_poolings 4  4   4   4   2    2    \
     --gdec_poolings 2  2   4   4   4    4    \
     --denc_fmaps    64 128 256 512 1024 2048 \
     --denc_poolings 4  4   4   4   2    2    \
-	--no_train_gen --no_bias \
+	--no_train_gen \
     --num_workers 8 --cache_dir data_tmp \
 
